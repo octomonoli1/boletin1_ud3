506960a65f48b76d9b822657273963106833fa3d
vricheb973 <vricheb973@g.educaand.es>
2025-11-26 12:49:31 +0100

    avances miercoles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -15,6 +16,11 @@ export class App {
   public notas: number[] =  [2,7,9,10,0,7,27,1,2,5];
   public edades: number[] = [18,19,19,19,26,24,20,21,35,28,45,46,59,12,18,17,19,20];
   public colores: string[] = ["verde","azul","rojo","amarillo","negro","blanco"];
+  public frutas: string[] = ["Aguacate", "Banana", "Chirimoya", "Dátil", "Fresa", "Guayaba", "Kiwi", "Limón", "Naranja", "Pera", "Sandía", "Uva", "Yuca"];
+  public fruta: string = "";
+  public matriz1: number[][] = this.inicializa_matriz();
+  public matriz2: number[][] = this.inicializa_matriz();
+
 
   public filtra_playas_pares(): string[]{
     return this.playas.filter((data, idx) => idx%2 == 0);
@@ -41,4 +47,46 @@ export class App {
   public elimina_color(color: string): string[]{
     return this.colores.filter(data => data !== color);
   }
+
+  public elimina_color_v2(color: string): void{
+    this.colores.splice(this.colores.indexOf(color),1);
+  }
+
+  public inserta_fruta(): void{
+    //Buscar donde insertarla
+    let encontrado = this.frutas.length;
+    for(let i = 0; i < this.frutas.length; i++){
+      if(this.fruta < this.frutas[i]){
+        encontrado = i;
+        break;
+      }
+    }
+
+    //Insertarla directamente ordenada
+    this.frutas.splice(encontrado, 0, this.fruta);
+  }
+
+  private inicializa_matriz(): number[][]{
+    let result: number[][] = [];
+
+    for(let i = 0; i < 3; i++){
+      let fila: number[] = [];
+      for(let j = 0; j < 3; j++){
+        fila.push(0);
+      }
+      result.push(fila);
+    }
+
+    this.imprime_matriz(result);
+    return result;
+  }
+
+  private imprime_matriz(matriz: number[][]): void{
+    for(let i = 0; i < 3; i++){
+      for(let j = 0; j < 3; j++){
+        console.log(matriz);
+      }
+    }
+  }
+
 }
